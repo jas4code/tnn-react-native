4718c006085dd5ac14b0bf099f00f3eb5352381f
jas4code <jasabocejo@protonmail.com>
2023-10-04 19:11:27 +0800

    Introduction to React-Native

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Header from './components/header';
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+			<Text>This is lesson3</Text>
 		</View>
 	);
 }
