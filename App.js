4eee5e8cc62314f7e0b5a9b1e0bea3a87907d3de
jas4code <jasabocejo@protonmail.com>
2023-10-05 22:25:53 +0800

    Alerts
    Adding alert foe case holder

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
@@ -18,12 +18,21 @@ export default function App() {
 	};
 
 	const submitHandler = (text) => {
-		setTodos((prevTodos) => {
-			return [
-				{ text: text, key: Math.random().toString() },
-				...prevTodos,
-			];
-		});
+		if (text.length > 3) {
+			setTodos((prevTodos) => {
+				return [
+					{ text: text, key: Math.random().toString() },
+					...prevTodos,
+				];
+			});
+		} else {
+			Alert.alert('OOPS!', 'Todos must be over 3 characters long.', [
+				{
+					text: 'Understood',
+					onPress: () => console.log('alert closed'),
+				},
+			]);
+		}
 	};
 
 	return (
