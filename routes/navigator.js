b9518414654d7ca5640188872b6ca5ff161fc8df
jas4code <jasabocejo@protonmail.com>
2024-02-25 02:15:09 +0800

    Customizing Navigation Options

--- routes/navigator.js
+++ routes/navigator.js
@@ -2,13 +2,23 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/home';
 import ReviewDetails from '../screens/reviewDetails';
+import { Pressable, Text } from 'react-native';
 
 const Stack = createStackNavigator();
 
 export default function Navigator() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName='Home'>
+			<Stack.Navigator
+				initialRouteName='Home'
+				screenOptions={{
+					headerTintColor: '#444',
+					headerStyle: {
+						backgroundColor: '#eee',
+						height: 60,
+					},
+				}}
+			>
 				<Stack.Screen name='Home' component={Home} />
 				<Stack.Screen name='Review Details' component={ReviewDetails} />
 			</Stack.Navigator>
